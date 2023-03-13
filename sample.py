6ea779327447f2fc4d223dfbc0040a21e286aa5f
root <root@ip-172-31-46-238.ap-south-1.compute.internal>
2023-03-13 04:23:03 +0000

    python lo print

---
+++ sample.py
@@ -0,0 +1,8 @@
+print("Poolu pooyu tharunam lokamlo evaru chodalede")
+print("Prema pongu samayam hrudayamlo evaru polchalede")
+print("Ninna mari gadiya chalalede")
+print("Nee odilo yugamu chalalede")
+print("Pedavi kadalalede")
+print("Ne jathalo kathala thalapu lede")
+print("Idi entooooo…")
+
